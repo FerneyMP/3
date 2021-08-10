ec104d54b376d03bc790200fb1545c8f0fdecb3a
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-08-09 23:33:47 -0500

    Add files via upload

---
+++ 3-main/lab3FinalM/lab3Final.pro
@@ -0,0 +1,15 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        funciones.cpp \
+        main.cpp \
+        metodo1.cpp \
+        metodo2.cpp
+
+HEADERS += \
+    funciones.h \
+    metodo1.h \
+    metodo2.h
---
+++ 3-main/lab3FinalM/metodo1.cpp
@@ -0,0 +1,17 @@
+#include "metodo1.h"
+/*
+unsigned long long size_text(const char *name)
+{
+
+}
+
+bool code_decode(const char *read, const char *write, unsigned int n, bool method)
+{
+
+}
+
+void read_text(const char *name, char *text, unsigned long long tam)
+{
+
+}
+*/
