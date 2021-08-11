49e714622a2e1ccdb071c7d9c7d9b36050196318
Mar115 <80101317+Mar115@users.noreply.github.com>
2021-08-11 06:27:05 -0500

    Add files via upload

---
+++ falta_metodo_1/funciones.h
@@ -0,0 +1,20 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void leer_menu();
+void crear_txt(string name);
+void escribir(string texto, string name, bool modo);
+string leer (string name, bool modo);
+void escribirCajero(string texto, string nombre, bool modo);
+
+
+bool ValidarDigitos (string numero , bool met);
+void validar(string &NUM, short int modo);
+
+#endif // FUNCIONES_H
